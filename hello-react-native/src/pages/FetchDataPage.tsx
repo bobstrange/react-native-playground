402b577a053d82d18b86ab133b8ca83e2ca4572a
bobstrange <bob.1983.g@gmail.com>
2020-10-27 23:40:55 +0900

    Fetching data

--- hello-react-native/src/pages/FetchDataPage.tsx
+++ hello-react-native/src/pages/FetchDataPage.tsx
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { StyleSheet, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import { MovieList } from '../components/MovieList'
 
 export const FetchDataPage = () => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true)
   const [data, setData] = useState([])
 
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      try {
+        const response = await axios.get('https://reactnative.dev/movies.json')
+        setData(response.data.movies)
+      } catch (error) {
+        console.error(error)
+      }
+      setLoading(false)
+    })()
+  }, [])
   return (
     <View style={styles.container}>
-      <MovieList data={data} />
+      {isLoading ? <ActivityIndicator /> : <MovieList data={data} />}
     </View>
   )
 }
