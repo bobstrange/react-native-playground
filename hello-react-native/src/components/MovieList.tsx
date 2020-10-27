b8bed123678d6b299ef346f714488b67d56c58ce
bobstrange <bob.1983.g@gmail.com>
2020-10-28 00:29:14 +0900

    Show movies

--- hello-react-native/src/components/MovieList.tsx
+++ hello-react-native/src/components/MovieList.tsx
@@ -1,25 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 
-const renderItem = (item) => {
+const renderItem = ({ item }) => {
   return (
     <Text>
       {item.title}, {item.releaseYear}
     </Text>
   )
 }
-export const MovieList = ({ data }) => {
-  return (
-    <View style={styles.container}>
-      <FlatList data={data} renderItem={renderItem} />
-    </View>
-  )
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-  },
-})
+export const MovieList = ({ data }) => {
+  return <FlatList data={data} renderItem={renderItem} />
+}
