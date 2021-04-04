6c274c93d2fc7b9ce36571e8d882dce9ddfd1a67
bobstrange <bob.1983.g@gmail.com>
2021-04-04 15:26:24 +0900

    tmp

---
+++ handson-ts/App.tsx
@@ -0,0 +1,77 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#222",
+  },
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  button: {
+    backgroundColor: "rgb(29, 161, 242)",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 20,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "900",
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    borderColor: "rgb(29, 161, 242)",
+    borderWidth: 2,
+    marginRight: 10,
+    borderRadius: 10,
+    color: "white",
+    paddingHorizontal: 10,
+    fontSize: 16,
+  },
+  content: {
+    padding: 20,
+  },
+  contentText: {
+    color: "white",
+    fontSize: 22,
+  },
+});
+
+export default function App() {
+  const [text, setText] = useState("");
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            onChangeText={(_text) => setText(_text)}
+          />
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>イートする</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="light" />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.contentText}>{text}</Text>
+      </View>
+      <StatusBar style="light" />
+    </SafeAreaView>
+  );
+}
