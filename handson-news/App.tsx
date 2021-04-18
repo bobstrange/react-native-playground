50943d6ee4813c3ed892a72fc9916b0d105e2f97
bobstrange <bob.1983.g@gmail.com>
2021-04-18 15:47:02 +0900

    feat: create news ListItem component

--- handson-news/App.tsx
+++ handson-news/App.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { ListItem } from './components/ListItem'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ListItem />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +17,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
