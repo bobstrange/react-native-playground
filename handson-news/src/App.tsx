f2873412afcd2d2bbc2b1ae7379ffcb8a64c3954
bobstrange <bob.1983.g@gmail.com>
2021-04-18 16:22:05 +0900

    feat: use flat list

--- handson-news/src/App.tsx
+++ handson-news/src/App.tsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
 import { ListItem } from './components/ListItem'
+
+const ARTICLES = [
+  {
+    urlToImage: 'https://picsum.photos/200',
+    title:
+      'React Native は、Facebookが開発したクロスプラットフォームアプリ開発用フレームワークです。 ReactNative でアプリを作ることで、クロスプラットフォーム( iOS / Androidどちらでも動く)アプリを作ることができます。',
+    author: 'Techニュース',
+  },
+  {
+    urlToImage: 'https://picsum.photos/201',
+    title:
+      'React Native Japan コミュニティは、日本でのReact Nativeの普及のため、活動を行っているコミュニティです。',
+    author: 'React Native Japan',
+  },
+  {
+    urlToImage: 'https://picsum.photos/203',
+    title:
+      'JavaScriptはプロトタイプベースのオブジェクト指向スクリプト言語であるが、クラスなどのクラスベースに見られる機能も取り込んでいます。',
+    author: 'JavaScriptFan',
+  },
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar />
-      <ListItem
-        title="React Native は、Facebookが開発したクロスプラットフォームアプリ開発用フレームワークです。 ReactNative でアプリを作ることで、クロスプラットフォーム( iOS / Androidどちらでも動く)アプリを作ることができます。"
-        subTitle="React Native Japan"
-        imageURL="https://picsum.photos/200"
+      <FlatList
+        data={ARTICLES}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.author}
+            imageURL={item.urlToImage}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
     </View>
   )
