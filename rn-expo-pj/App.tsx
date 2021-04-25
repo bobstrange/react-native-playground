e372fc70b7cef182a907b55e4fb4f4d67b052799
bobstrange <bob.1983.g@gmail.com>
2021-04-25 23:26:30 +0900

    feat: styling component

--- rn-expo-pj/App.tsx
+++ rn-expo-pj/App.tsx
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, SafeAreaView, View, StyleSheet } from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Here are some boxes of different colours
+        </Text>
+        <View style={styles.cyanBox}>
+          <Text style={styles.boxText}>Cyan: #2aa198</Text>
+        </View>
+        <View style={styles.blueBox}>
+          <Text style={styles.boxText}>Blue: #268bd2</Text>
+        </View>
+        <View style={styles.magentaBox}>
+          <Text style={styles.boxText}>Magenta: #d33682</Text>
+        </View>
+        <View style={styles.orangeBox}>
+          <Text style={styles.boxText}>Orange: #cb4b16</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  container: {
+    paddingHorizontal: 10,
+    paddingTop: 40,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  cyanBox: {
+    backgroundColor: '#2aa198',
+    padding: 10,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  blueBox: {
+    backgroundColor: '#268bd2',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  magentaBox: {
+    backgroundColor: '#d33682',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  orangeBox: {
+    backgroundColor: '#cb4b16',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
+
+export default App
