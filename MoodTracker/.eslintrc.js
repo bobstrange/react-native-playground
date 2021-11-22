752a4d5603d786e1e44ce403356ab544bf3926b3
bobstrange <bob.1983.g@gmail.com>
2021-11-22 22:37:11 +0900

    fix: eslint and prettier config

--- MoodTracker/.eslintrc.js
+++ MoodTracker/.eslintrc.js
@@ -1,4 +1,8 @@
+/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    semi: 'off',
+  },
+}
